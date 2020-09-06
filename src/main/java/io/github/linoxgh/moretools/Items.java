3e730cccdca66e61cd759f7fe43744184ef48009
LinoxGH <54643600+LinoxGH@users.noreply.github.com>
2020-09-06 14:58:09 +0300

    Added Crescent Hammer.

---
+++ src/main/java/io/github/linoxgh/moretools/Items.java
@@ -0,0 +1,11 @@
+package io.github.linoxgh.moretools;
+
+import org.bukkit.Material;
+
+import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
+
+public class Items {
+    
+    // TOOLS
+    public static final SlimefunItemStack CRESCENT_HAMMER = new SlimefunItemStack("CRESCENT_HAMMER", Material.IRON_HOE, "&bCrescent Hammer", "", "&7Actually this is a wrench, really.");
+}
