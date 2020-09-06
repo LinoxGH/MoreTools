42dcf12126a2870fecbb013bfe1682ac1f6be27a
LinoxGH <54643600+LinoxGH@users.noreply.github.com>
2020-09-06 08:46:24 +0300

    Changing file name.

--- src/main/java/io/github/linoxgh/MoreTools/MoreTools.java
+++ src/main/java/io/github/linoxgh/MoreTools/MoreTools.java
@@ -1,4 +1,4 @@
-package me.CHANGEME.slimefunaddon;
+package io.github.linoxgh.MoreTools;
 
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
@@ -14,7 +14,7 @@ import me.mrCookieSlime.Slimefun.bstats.bukkit.Metrics;
 import me.mrCookieSlime.Slimefun.cscorelib2.config.Config;
 import me.mrCookieSlime.Slimefun.cscorelib2.item.CustomItem;
 
-public class ExampleAddon extends JavaPlugin implements SlimefunAddon {
+public class MoreTools extends JavaPlugin implements SlimefunAddon {
 
     @Override
     public void onEnable() {
