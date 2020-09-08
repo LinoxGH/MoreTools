91deaba534fb394556ba8e406041e43453c8b8f1
LinoxGH <54643600+LinoxGH@users.noreply.github.com>
2020-09-08 08:18:46 +0300

    Added some config options for Crescent Hammer 2/3

--- src/main/java/io/github/linoxgh/moretools/items/CrescentHammer.java
+++ src/main/java/io/github/linoxgh/moretools/items/CrescentHammer.java
@@ -10,7 +10,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
 
-import me.mrCookieSlime.Slimefun.cscorelib2.protection.ProtectableAction;
+import io.github.linoxgh.moretools.Messages;
+
 import io.github.thebusybiscuit.slimefun4.core.attributes.DamageableItem;
 import io.github.thebusybiscuit.slimefun4.core.attributes.EnergyNetComponent;
 import io.github.thebusybiscuit.slimefun4.core.handlers.BlockBreakHandler;
@@ -27,6 +28,7 @@ import me.mrCookieSlime.Slimefun.Objects.Category;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
 import me.mrCookieSlime.Slimefun.api.BlockStorage;
 import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
+import me.mrCookieSlime.Slimefun.cscorelib2.protection.ProtectableAction;
 
 /**
  * A {@link CrescentHammer} is a {@link SlimefunItem} which allows you to break placed machine blocks
@@ -37,8 +39,12 @@ import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
  */
 public class CrescentHammer extends SimpleSlimefunItem<ItemUseHandler> implements DamageableItem {
 
+    private boolean damageable = true;
+
     public CrescentHammer(Category category, SlimefunItemStack item, RecipeType recipeType, ItemStack[] recipe) {
         super(category, item, recipeType, recipe);
+        
+        damageable = MoreTools.getInstance().getCfg().getBoolean("item-settings.crescent-hammer.damageable");
     }
     
     @Override
@@ -47,21 +53,26 @@ public class CrescentHammer extends SimpleSlimefunItem<ItemUseHandler> implement
             Optional<Block> block = e.getClickedBlock();
             if (block.isPresent()) {
                 Block b = block.get();
+                Player p = e.getPlayer();
                 
-                if (SlimefunPlugin.getProtectionManager().hasPermission(e.getPlayer(), b.getLocation(), ProtectableAction.BREAK_BLOCK)) {
+                if (SlimefunPlugin.getProtectionManager().hasPermission(p, b.getLocation(), ProtectableAction.BREAK_BLOCK)) {
                     
-                   SlimefunItem sfItem = BlockStorage.check(b);
-                   if (sfItem != null) {
-                       if (sfItem instanceof EnergyNetComponent || sfItem instanceof EnergyRegulator || sfItem.getID().startsWith("CARGO_NODE") || sfItem instanceof CargoManager || sfItem instanceof ReactorAccessPort || sfItem instanceof TrashCan) {
+                    SlimefunItem sfItem = BlockStorage.check(b);
+                    if (sfItem != null) {
+                        if (sfItem instanceof EnergyNetComponent || sfItem instanceof EnergyRegulator || sfItem.getID().startsWith("CARGO_NODE") || sfItem instanceof CargoManager || sfItem instanceof ReactorAccessPort || sfItem instanceof TrashCan) {
                        
-                           Player p = e.getPlayer();
-                           BlockBreakEvent event = new BlockBreakEvent(b, p);
-                           Bukkit.getPluginManager().callEvent(event);
-                           b.getWorld().playEffect(b.getLocation(), Effect.STEP_SOUND, b.getType()); //TODO Make this configurable
-                           damageItem(p, e.getItem()); //TODO Make this configurable
-                       }
-                   }
-                   //TODO Send fail message.
+                            BlockBreakEvent event = new BlockBreakEvent(b, p);
+                            Bukkit.getPluginManager().callEvent(event);
+                            if (!event.isCancelled()) {
+                                b.getWorld().playEffect(b.getLocation(), Effect.STEP_SOUND, b.getType());
+                               
+                                if (isDamageable()) {
+                                    damageItem(p, e.getItem());
+                                }
+                            }
+                        }
+                    }
+                    p.sendMessage(Messages.ITEMS$CRESCENT_HAMMER$RIGHT_CLICK_FAIL);
                 }
             }
             e.cancel();
@@ -75,6 +86,7 @@ public class CrescentHammer extends SimpleSlimefunItem<ItemUseHandler> implement
             public boolean onBlockBreak(BlockBreakEvent e, ItemStack item, int fortune, List<ItemStack> drops) {
                 if (isItem(item)) {
                     e.setCancelled(true);
+                    e.getPlayer().sendMessage(Messages.ITEMS$CRESCENT_HAMMER$BLOCK_BREAKING);
                     return true;
                 }
                 return false;
@@ -95,7 +107,7 @@ public class CrescentHammer extends SimpleSlimefunItem<ItemUseHandler> implement
     
     @Override
     public boolean isDamageable() {
-        return true;
+        return damageable;
     }
     
 }
