b01f2f54be26c43399f7b83df28b4c152e7c31ad
LinoxGH <54643600+LinoxGH@users.noreply.github.com>
2020-09-08 08:32:42 +0300

    Fixed compilation errors.

--- src/main/java/io/github/linoxgh/moretools/Messages.java
+++ src/main/java/io/github/linoxgh/moretools/Messages.java
@@ -6,14 +6,14 @@ import me.mrCookieSlime.Slimefun.cscorelib2.config.Config;
 
 public enum Messages {
 
-    ITEMS$CRESCENT_HAMMER$BLOCK_BREAKING;
+    ITEMS$CRESCENT_HAMMER$BLOCK_BREAKING,
     ITEMS$CRESCENT_HAMMER$RIGHT_CLICK_FAIL;
     
     private static Config cfg = null;
     
     private final String message;
     
-    public Messages() {
+    Messages() {
         message = ChatColor.translateAlternateColorCodes('&', cfg.getString("messages" + name().toLowerCase().replace('_', '-').replace('$', '.')));
     }
     
