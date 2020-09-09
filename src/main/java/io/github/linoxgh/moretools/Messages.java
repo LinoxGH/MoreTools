3c1cb955e93d5db415cf3c835a0a0bb23f9baab3
LinoxGH <54643600+LinoxGH@users.noreply.github.com>
2020-09-09 14:11:13 +0300

    Fixed an issue.

--- src/main/java/io/github/linoxgh/moretools/Messages.java
+++ src/main/java/io/github/linoxgh/moretools/Messages.java
@@ -7,7 +7,9 @@ public enum Messages {
 
     CRESCENTHAMMER_BLOCKBREAKING("items.crescent-hammer.block-breaking"),
     CRESCENTHAMMER_DISMANTLEFAIL("items.crescent-hammer.dismantle-fail"),
-    CRESCENTHAMMER_ROTATEFAIL("items.crescent-hammer.rotate-fail");
+    CRESCENTHAMMER_ROTATEFAIL("items.crescent-hammer.rotate-fail"),
+    CRESCENTHAMMER_CHANNELCHANGEFAIL("items.crescent-hammer.channel-change-fail"),
+    CRESCENTHAMMER_CHANNELCHANGESUCCESS("items.crescent-hammer.channel-change-success");
     
     private final String message;
     
